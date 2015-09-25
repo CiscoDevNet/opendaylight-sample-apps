4116a58accb5bbdfa97093fdc870b97c53bea151
alzverev <alzverev@cisco.com>
2015-09-25 13:47:21 -0700

    customization

--- next-tooltip-customization/js/next-modules/tooltip.js
+++ next-tooltip-customization/js/next-modules/tooltip.js
@@ -1,85 +1,85 @@
 (function (nx, global) {
-				// node tooltip class
-				// see nx.graphic.Topology.Node reference to learn what node's properties you're able to use
-				nx.define('TooltipNode', nx.ui.Component, {
-								properties: {
-												'node': {}, // NeXt automatically provides you the access to a selected nx.graphic.Topology.Node instance
-												'topology': {}, // NeXt also provides you the access to a topology object
-												'newNodeName': '' // we're gonna store a new node's name over here
-								},
-								// 'view' defines the appearance of the tooltip
-								view: {
-												content: {
-																content: [
-																				{
-																								tag: 'h1',
-																								content: '{#node.label}'
-																				},
-																				{
-																								tag: 'label',
-																								content: 'New node name:'
-																				},
-																				{
-																								tag: 'input',
-																								content: '{#newNodeName}',
-																								props: {
-																												'placeholder': 'Input a new name...',
-																												'class': 'input form-control'
-																								}
-																				},
-																				{
-																								tag: 'button',
-																								content: 'Save',
-																								props: {
-																												'class': 'btn btn-success'
-																								},
-																								events: {
-																												'click': '{#onSaveName}'
-																								}
-																				},
-																				{
-																								tag: 'button',
-																								content: {
-																												content: 'Remove'
-																								},
-																								events: {
-																												'click': '{#onRemoveNode}'
-																								},
-																								props: {
-																												'class': 'btn btn-danger'
-																								}
-																				}],
-																props: {
-																				'class': 'custom-tooltip'
-																}
-												}
-								},
-								methods:	{
-												'onRemoveNode':	function (sender, event) {
-																// todo: program node removal
-																// how do i get links/nodes/topology object?
-												}
-												,
-												'onSaveName':	function () {
-																// todo: program saving the name
-																alert(1);
-												}
-								}
-				})
-				;
-				// link tooltip class
-				// see nx.graphic.Topology.Link reference to learn what link's properties you're able to use
-				nx.define('TooltipLink', nx.ui.Component, {
-								properties: {
-												node: {},
-												link: {},
-												topology: {}
-								},
-								view: {
-												content: [
-																{}
-												]
-								}
-				});
+ // node tooltip class
+ // see nx.graphic.Topology.Node reference to learn what node's properties you're able to use
+ nx.define('TooltipNode', nx.ui.Component, {
+  properties: {
+   'node': {}, // NeXt automatically provides you the access to a selected nx.graphic.Topology.Node instance
+   'topology': {}, // NeXt also provides you the access to a topology object
+   'newNodeName': '' // we're gonna store a new node's name over here
+  },
+  // 'view' defines the appearance of the tooltip
+  view: {
+   content: {
+	content: [
+	 {
+	  tag: 'h1',
+	  content: '{#node.label}'
+	 },
+	 {
+	  tag: 'label',
+	  content: 'New node name:'
+	 },
+	 {
+	  tag: 'input',
+	  content: '{#newNodeName}',
+	  props: {
+	   'placeholder': 'Input a new name...',
+	   'class': 'input form-control'
+	  }
+	 },
+	 {
+	  tag: 'button',
+	  content: 'Save',
+	  props: {
+	   'class': 'btn btn-success'
+	  },
+	  events: {
+	   'click': '{#onSaveName}'
+	  }
+	 },
+	 {
+	  tag: 'button',
+	  content: {
+	   content: 'Remove'
+	  },
+	  events: {
+	   'click': '{#onRemoveNode}'
+	  },
+	  props: {
+	   'class': 'btn btn-danger'
+	  }
+	 }],
+	props: {
+	 'class': 'custom-tooltip'
+	}
+   }
+  },
+  methods: {
+   'onRemoveNode': function (sender, event) {
+	// todo: program node removal
+	// how do i get links/nodes/topology object?
+   }
+   ,
+   'onSaveName': function () {
+	// todo: program saving the name
+	alert(1);
+   }
+  }
+ })
+ ;
+ // link tooltip class
+ // see nx.graphic.Topology.Link reference to learn what link's properties you're able to use
+ nx.define('TooltipLink', nx.ui.Component, {
+  properties: {
+   node: {},
+   link: {},
+   topology: {}
+  },
+  view: {
+   content: [
+	{}
+   ]
+  }
+ });
 })
 (nx, nx.global);
--- next-tooltip-customization/js/next-modules/topology.js
+++ next-tooltip-customization/js/next-modules/topology.js
@@ -1,51 +1,51 @@
 (function (nx, global) {
-				nx.define('TopologyContainer', nx.ui.Component, {
-								properties: {
-												topology: {
-																get: function () {
-																				return this.view('topology');
-																}
-												}
-								},
-								view: {
-												content: [
-																{
-																				'name': 'topology',
-																				'type': 'nx.graphic.Topology',
-																				'props': {
-																								'adaptive': true, // width 100% if true
-																								'showIcon': true,
-																								'nodeConfig': {
-																												'label': 'model.name',
-																												/*
-																													icon types:
-																													https://developer.cisco.com/media/neXt-site/example.html#Basic/icons
-																													*/
-																												'iconType': 'router',
-																												'color': '#0how00'
-																								},
-																								'linkConfig': {
-																												'linkType': 'curve',// also: parallel
-																												'style': {
-																																//'stroke-dasharray': '5, 5' // 'a, b' - a is the length of a stroke; b is a length of
-																																// a gap
-																												}
-																								},
-																								tooltipManagerConfig: {
-																												nodeTooltipContentClass: 'TooltipNode',
-																												linkTooltipContentClass: 'TooltipLink'
-																								},
-																								'data': topologyData,
-																								'identityKey': 'id', // helps to link source and target
-																								'width': 800,
-																								'height': 400,
-																								'dataProcessor': 'force', // arrange nodes positions if not set
-																								'enableSmartLabel': true, // moves the labels in order to avoid overlay of them
-																								'enableGradualScaling': true, // may slow down, if true
-																								'supportMultipleLink': true // if true, two nodes can have more than one link
-																				}
-																}
-												]
-								}
-				});
+ nx.define('TopologyContainer', nx.ui.Component, {
+  properties: {
+   topology: {
+	get: function () {
+	 return this.view('topology');
+	}
+   }
+  },
+  view: {
+   content: [
+	{
+	 'name': 'topology',
+	 'type': 'nx.graphic.Topology',
+	 'props': {
+	  'adaptive': true, // width 100% if true
+	  'showIcon': true,
+	  'nodeConfig': {
+	   'label': 'model.name',
+	   /*
+		icon types:
+		https://developer.cisco.com/media/neXt-site/example.html#Basic/icons
+		*/
+	   'iconType': 'router',
+	   'color': '#0how00'
+	  },
+	  'linkConfig': {
+	   'linkType': 'curve',// also: parallel
+	   'style': {
+		//'stroke-dasharray': '5, 5' // 'a, b' - a is the length of a stroke; b is a length of
+		// a gap
+	   }
+	  },
+	  tooltipManagerConfig: {
+	   nodeTooltipContentClass: 'TooltipNode',
+	   linkTooltipContentClass: 'TooltipLink'
+	  },
+	  'data': topologyData,
+	  'identityKey': 'id', // helps to link source and target
+	  'width': 800,
+	  'height': 400,
+	  'dataProcessor': 'force', // arrange nodes positions if not set
+	  'enableSmartLabel': true, // moves the labels in order to avoid overlay of them
+	  'enableGradualScaling': true, // may slow down, if true
+	  'supportMultipleLink': true // if true, two nodes can have more than one link
+	 }
+	}
+   ]
+  }
+ });
 })(nx, nx.global);
