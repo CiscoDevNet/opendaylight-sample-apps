f41350b94bb0880f01231df731a6a681cb26c4d4
Alexei Zverev <alzverev@cisco.com>
2015-09-28 15:41:08 -0700

    minor fixes & comments
    i added a few comments and made minor fixes

--- next-topologydata-export/js/shell.js
+++ next-topologydata-export/js/shell.js
@@ -2,7 +2,7 @@
 	// initialize a new application instance
 	var app = new nx.ui.Application();
 	/* TopologyContainer is a nx.ui.Component object that can contain much more things than just a nx.graphic.Topology object.
-	 We can 'inject' a topology instance inside and interact with it easily
+	 We can 'inject' a topology instance towards and interact with it easily
 	 */
 	var topologyContainer = new TopologyContainer();
 	var topology = topologyContainer.topology();
@@ -11,9 +11,7 @@
 	actionBar.assignTopology(topology);
 	//assign the app to a <div>
 	app.container(document.getElementById('next-app'));
-	// feed topology object with nodes and links...
-	topology.data();
-	// ... then attach the topology to the app instance
+	// attach the topology to the app instance
 	topology.attach(app);
 	// attach an action bar
 	actionBar.attach(app);
