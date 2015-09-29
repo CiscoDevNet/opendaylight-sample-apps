311350ff19775228bd70ccd111e1a75d7ca3fc18
Alexei Zverev <alzverev@cisco.com>
2015-09-28 18:40:08 -0700

    clarification & comments

--- next-odl-integration/js/next-modules/data.js
+++ next-odl-integration/js/next-modules/data.js
@@ -1,15 +1,20 @@
-var topologyData;
+// define empty topology data object
+var topologyData = {};
 // function transforms odl respond to next json format
 var odl2next = function (data) {
+	// create alias
 	var topology = data.topology[0];
+	// prepare stub for results
 	var topologyResult = {nodes: [], links: []};
-	// processing nodes
+	// process nodes
 	for (var i = 0; i < topology.node.length; i++) {
 		var node = {};
 		// node name
 		if (topology.node[i].hasOwnProperty('node-id')) {
 			node.name = topology.node[i]['node-id'];
 		}
+		// ... other actions with node object ...
+		// add the node to the result object
 		topologyResult.nodes.push(node);
 	}
 	// processing links
@@ -19,8 +24,8 @@ var odl2next = function (data) {
 			source: topology.link[i].source['source-node'],
 			target: topology.link[i].destination['dest-node']
 		};
+		// add the link to the result object
 		topologyResult.links.push(link);
 	}
 	return topologyResult;
 };
-
