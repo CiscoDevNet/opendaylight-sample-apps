4ab10fb43ec59a94f91c623784c88ece5033388a
Giles Heron <giheron@cisco.com>
2017-06-14 17:56:33 +0100

    update add_route.py for Boron

--- odl-bgp/add_route.py
+++ odl-bgp/add_route.py
@@ -3,16 +3,24 @@ from route_attributes import attributes
 from v4_uni_app_route import app_route
 import sys
 
-controller = sys.argv[1]
-prefix = sys.argv[2]
+if __name__ == "__main__":
+    # check args length
+    if (len(sys.argv) != 3):
+        print "usage %s ODL_IP_address IP_Prefix" % sys.argv[0]
+        sys.exit(1)
 
-# initialise route object
-route = app_route(controller)
+    controller = sys.argv[1]
+    prefix = sys.argv[2]
 
-# set route attributes
-attrib = attributes()
-attrib.set_communities("100:100")
-attrib.set_next_hop(controller)
+    # initialise route object
+    route = app_route(controller)
 
-# advertise route
-route.put_app_route(prefix, attrib.get())
+    # set route attributes
+    attrib = attributes()
+    attrib.set_communities("100:100")
+    attrib.set_next_hop(controller)
+
+    # advertise route
+    route.put_app_route(prefix, attrib.get())
+
+    print "%s advertised" % sys.argv[2]
